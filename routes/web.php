d5af9b91d8a87861cca01b95ef22ffdc76e691fe
BuiHa2025 <kuti941997@gmail.com>
2026-06-18 23:30:31 +0700

    fix: add admin routes for categories and brands

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,14 @@ use App\Models\Brand;
 use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\BrandController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('admin')->group(function () {
+    Route::resource('categories', CategoryController::class);
+    Route::resource('brands', BrandController::class);
 });
