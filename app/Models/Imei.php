de5a54d7b7899c49cc6f0e5871f1e251dae32368
XLshin <hnguyenquoc2005@gmail.com>
2026-06-19 00:32:15 +0700

    Resolve merge conflict

--- app/Models/Imei.php
+++ app/Models/Imei.php
@@ -4,16 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Imei extends Model
+class IMEI extends Model
 {
+    protected $table = 'imeis';
+
     protected $fillable = [
         'product_variant_id',
         'imei',
         'status'
     ];
 
-        public function productVariant()
-        {
-            return $this->belongsTo(ProductVariant::class);
-        }
+    public function productVariant()
+    {
+        return $this->belongsTo(ProductVariant::class);
+    }
+
+    public function warranties()
+    {
+        return $this->hasMany(Warranty::class);
+    }
 }
