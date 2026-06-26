1cc7bc07de339e281ce602ef436b6d3119299271
XLshin <hnguyenquoc2005@gmail.com>
2026-06-26 22:36:06 +0700

    cập nhật lại phần thông tin hiển thị kho và nhập kho

--- app/Models/Imei.php
+++ app/Models/Imei.php
@@ -19,8 +19,8 @@ class Imei extends Model
         return $this->belongsTo(ProductVariant::class);
     }
 
-    public function warranties()
+    public function warranty()
     {
-        return $this->hasMany(Warranty::class);
+        return $this->hasOne(Warranty::class);
     }
 }
